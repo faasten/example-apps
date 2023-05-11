905ad87c7058913146c3a0e01b01ee29cc7b4218
Yue Tan <yuetan@cs.princeton.edu>
2023-05-11 07:56:51 -0400

    tutorial thumbnail

--- tutorial/thumbnail/workload.py
+++ tutorial/thumbnail/workload.py
@@ -1,15 +1,19 @@
 from PIL import Image
 
 def handle(event, cloudcall):
-    download_path = event['input']
-    output_dir = event['output_dir']
-    sizes = event['sizes']
+    download_path = event['input']['download_key']
+    output_dir = event['input']['output_dir']
+    sizes = event['input']['sizes']
+    blobs = {}
     with cloudcall.fs_openblob(download_path) as blob:
         img = Image.open(blob)
         for size in sizes:
             img.thumbnail(tuple(size))
-            thumnail_name = '-'.join(size) + '.jpeg'
-            thumbnail_path = ':'.join([output_dir, thumbnail_name])
-            with cloudcall.create_blob(thumbnail_path) as newblob:
+            thumbnail_name = '-'.join(map(str, size)) + '.jpeg'
+            upload_path = ':'.join([output_dir, thumbnail_name])
+            with cloudcall.create_blob() as newblob:
                 img.save(newblob, format='JPEG')
-                newblob.finalize(b'')
+                bn = newblob.finalize(b'')
+                blobs[thumbnail_name] = bn
+                cloudcall.fs_linkblob(upload_path, bn)
+    return {'thumb-blobs': blobs}
