af7e3a6adc8036babd81dae9af342383a1eb7cbc
Yue Tan <yuetan@cs.princeton.edu>
2023-05-11 11:09:05 -0400

    update detect-face/workload.py

--- tutorial/detect-face/workload.py
+++ tutorial/detect-face/workload.py
@@ -3,5 +3,9 @@ from io import BytesIO
 import face_recognition
 
 def handle(request, syscall):
-    # complete
-    return {}
+    download_path = event['input']['download_key']
+    output_dir = event['input']['output_dir']
+    blobs = {}
+    # complete this function so that it uploads detected faces as blobs to
+    # PyPI page of face_recognition: https://pypi.org/project/face-recognition/
+    return {'face-blobs': blobs}
